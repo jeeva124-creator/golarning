a515a680f292f5acf1d8ff7a04463d5a432f95f5
jeeva124-creator <jeevapopz6@gmail.com>
2026-03-11 23:23:12 -0400

    add new

--- Error_Handiling/main.go
+++ Error_Handiling/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	return a / b, nil
+}
+
 func main() {
 
+	//GO dont use expations for normal failures
+	//   function  =return errors as normal return value
+   if  err :
+}
+
+func run() error {
+	intput := "3"
+	level, err := parsLevel(intput)
+	if err != nil {
+		return err
+	}
+	fmt.Println("selected level", level)
+	return nil
+}
+func parsLevel(s string) (int, error) {
+	n, err := strconv.Atoi(s)
+
+	if err != nil {
+		return 0, fmt.Errorf("level mustbe number")
+	}
+	if n < 1 || n > 5 {
+		return 0, fmt.Errorf("Level must be a 1 and 5")
+	}
+	return n, nil
 }
